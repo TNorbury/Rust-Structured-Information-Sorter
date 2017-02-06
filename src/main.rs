2b342ae5bdabdb5985e2c3b87dc3780d5f1ac93d
TNorbury <tylernorbury96@gmail.com>
2017-02-06 14:44:19 -0800

    Add comparison features for the person struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::BTreeSet;
+use std::cmp::Ordering;
+
+
+#[derive(Eq)]
+struct Person 
+{
+    name: &'static str,
+    age: u8, //Assuming people won't be older than 255 or have a negative age.
+}
+
+
+//Implement functionality for comparing Person structs to one another. This is
+//required in order to put Persons into an order collection (like a BTreeSet).
+impl Ord for Person 
+{
+    fn cmp(&self, other: &Person) -> Ordering 
+    {
+        self.name.cmp(&other.name)
+    }
+}
+
+impl PartialOrd for Person
+{
+    fn partial_cmp(&self, other: &Person) -> Option<Ordering>
+    {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Person
+{
+    fn eq(&self, other: &Person) -> bool
+    {
+        self.name == other.name
+    }
+}
+
+fn main() 
+{
+    //Create a BTreeSet (this will ensure that the collection is ordered)
+    //that will contain all the people.
+    let mut people = BTreeSet::new();
+
+    let mut person1 = Person {name: "Tyler Norbury", age: 20};
+
+    people.insert(person1);
 }
